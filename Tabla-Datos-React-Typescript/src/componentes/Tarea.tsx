dc85d9df0849ebf48fe308e766e0e38a4291ee07
felixstiven <felixstiven12@gmail.com>
2024-10-23 07:49:29 -0500

    actualizando codigo typescript y react

---
+++ Tabla-Datos-React-Typescript/src/componentes/Tarea.tsx
@@ -0,0 +1,19 @@
+
+
+type Tarea = {
+    tarea: string
+    borrarTarea: () => void
+}
+
+export const Tarea = ({tarea, borrarTarea}: Tarea) => {
+    return (
+      <div className="task">
+        <span>
+            {tarea}
+        </span>
+        <button  onClick={borrarTarea}>
+            Borrar
+        </button>
+      </div>
+    )
+}
---
+++ Tabla-Datos-React-Typescript/src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM  from 'react-dom/client'
+import './componentes/TodoApp'
+import './style.css'
+import { TodoApp } from './componentes/TodoApp'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+      <TodoApp></TodoApp>
+  </React.StrictMode>,
+)
